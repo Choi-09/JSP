345f50272bb2de460ef4e2d2f991f1b031d79a53
Jeongin Choi <51871037+Choi-09@users.noreply.github.com>
2022-11-21 23:00:04 +0900

    Update ViewController.java

--- MustHaveJSP/java/model2/mvcboard/ViewController.java
+++ MustHaveJSP/java/model2/mvcboard/ViewController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/mvcboard/view.do")
 public class ViewController extends HttpServlet {
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp)
+    protected void service(HttpServletRequest req, HttpServletResponse resp) 
         throws ServletException, IOException {
         // 게시물 불러오기
         MVCBoardDAO dao = new MVCBoardDAO();
@@ -27,3 +27,6 @@ public class ViewController extends HttpServlet {
         req.getRequestDispatcher("/14MVCBoard/View.jsp").forward(req, resp);
     }
 }
+
+//클라이언트 요청에 따라 서블릿 컨테이너는 service() 메서드를 호출하고, service() 메서드는 요청이 GET인지 POST인지 구분하여 각각 doGet(), doPost() 메서드를 호출한다.
+//HttpServlet 클래스에서 사용자 요청을 처리하는 doGet/doPost 메서드는 모두 HttpServletRequest와 HttpServletResponse 객체를 매개변수로 가지고 있다.
